cd7456942da34673755207a7c409f704d34beced
boytur <mask30254@gmail.com>
2025-01-26 22:02:32 +0700

    ✨ feat[config]: Add environment configuration file and update server initialization to use env variables

---
+++ server/src/config/env.config.ts
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+
+// Load environment variables from the `.env` file
+dotenv.config();
+
+interface DatabaseConfig {
+  url: string;
+  user: string;
+  password: string;
+  name: string;
+  port: number;
+}
+
+interface PgAdminConfig {
+  email: string;
+  password: string;
+  port: number;
+}
+
+interface AppConfig {
+  appPort: number;
+  appMode?: string;
+  database: DatabaseConfig;
+  pgAdmin: PgAdminConfig;
+}
+
+const envConfig: AppConfig = {
+  appPort: parseInt(process.env.APP_PORT || "3000", 10),
+  appMode: process.env.NODE_ENV || "development",
+  database: {
+    url: process.env.DATABASE_URL || "",
+    user: process.env.PG_USER || "default_user",
+    password: process.env.PG_PASSWORD || "default_password",
+    name: process.env.PG_DATABASE || "default_db",
+    port: parseInt(process.env.DB_PORT || "5432", 10),
+  },
+  pgAdmin: {
+    email: process.env.PGADMIN_EMAIL || "admin@example.com",
+    password: process.env.PGADMIN_PASSWORD || "default_password",
+    port: parseInt(process.env.PGADMIN_PORT || "5050", 10),
+  },
+};
+
+export default envConfig;
