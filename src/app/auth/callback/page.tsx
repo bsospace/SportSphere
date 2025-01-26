8abcdf3b552c927ede1d4dbc69050e8aac85de40
boytur <mask30254@gmail.com>
2025-01-27 03:59:50 +0700

    🐛 fix[window is not defined]: Refactor authentication callback to use useEffect and add axios interceptor for dynamic token handling

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -2,33 +2,36 @@
 /* eslint-disable @typescript-eslint/no-unused-vars*/
 /* eslint-disable @typescript-eslint/no-empty-object-type*/
 "use client";
+import { useEffect } from 'react';
+
 type Props = {}
 
 function Callback ({}: Props) {
-  // get access token and refresh token from url query parameters
-  const urlParams = new URLSearchParams(window.location.search)
+  useEffect(() => {
+    // get access token and refresh token from url query parameters
+    const urlParams = new URLSearchParams(window.location.search)
 
-  const accessToken = urlParams.get('accessToken')
-  const refreshToken = urlParams.get('refreshToken')
+    const accessToken = urlParams.get('accessToken')
+    const refreshToken = urlParams.get('refreshToken')
 
-  console.log(accessToken)
-  console.log(refreshToken)
-  
+    console.log(accessToken)
+    console.log(refreshToken)
+    
+    // set access token to local storage
+    if (accessToken) {
+      localStorage.setItem('accessToken', accessToken)
+    }
+    if (refreshToken) {
+      localStorage.setItem('refreshToken', refreshToken)
+    }
 
-  // set access token to local storage
-  if (accessToken) {
-    localStorage.setItem('accessToken', accessToken)
-  }
-  if (refreshToken) {
-    localStorage.setItem('refreshToken', refreshToken)
-  }
+    if(!accessToken || !refreshToken){
+      window.location.href = '/login'
+    }
 
-  if(!accessToken || !refreshToken){
-    window.location.href = '/login'
-  }
-
-  alert(accessToken)
-  window.location.href = '/'
+    alert(accessToken)
+    window.location.href = '/'
+  }, [])
 
   return (
     <div>
