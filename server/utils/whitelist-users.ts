6cdb226593022da73f29a124d09b35a30e6a75ea
boytur <mask30254@gmail.com>
2025-01-27 00:24:00 +0700

    ✨ feat[whitelist]: Add initial whitelist of user emails for access control

---
+++ server/utils/whitelist-users.ts
@@ -0,0 +1,47 @@
+export const WhiteListUsers = [
+    // BSO
+    "65160090@go.buu.ac.th",
+    "65160326@go.buu.ac.th",
+    "65160337@go.buu.ac.th",
+    "65160340@go.buu.ac.th",
+  
+    // Staff
+    "67160449@go.buu.ac.th",
+    "67160162@go.buu.ac.th",
+    "67160191@go.buu.ac.th",
+    "67160024@go.buu.ac.th",
+    "66160273@go.buu.ac.th",
+    "67160147@go.buu.ac.th",
+    "66160386@go.buu.ac.th",
+    "67160433@go.buu.ac.th",
+    "66160170@go.buu.ac.th",
+    "67160406@go.buu.ac.th",
+    "67160400@go.buu.ac.th",
+    "67160386@go.buu.ac.th",
+    "66160110@go.buu.ac.th",
+    "67160192@go.buu.ac.th",
+    "67160004@go.buu.ac.th",
+    "67160014@go.buu.ac.th",
+    "67160195@go.buu.ac.th",
+    "67160109@go.buu.ac.th",
+    "67160250@go.buu.ac.th",
+    "67160075@go.buu.ac.th",
+    "66160395@go.buu.ac.th",
+    "67160070@go.buu.ac.th",
+    "67160087@go.buu.ac.th",
+    "67160009@go.buu.ac.th",
+  
+    // SMO
+    "65160055@go.buu.ac.th",
+    "65160156@go.buu.ac.th",
+    "66160262@go.buu.ac.th",
+    "66160021@go.buu.ac.th",
+    "65160209@go.buu.ac.th",
+    "66160351@go.buu.ac.th",
+    "65160216@go.buu.ac.th",
+    "66160092@go.buu.ac.th",
+    "66160349@go.buu.ac.th",
+    "66160353@go.buu.ac.th",
+    "66160361@go.buu.ac.th"
+  ];
+  
