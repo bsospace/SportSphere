f48c6daa2064da9b7137f26bdfbc7fe4c77ecc61
boytur <mask30254@gmail.com>
2025-01-27 00:23:30 +0700

    ✨ feat[cors]: Update CORS configuration to use environment-specific origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 app.use(cors({
-  origin: '*',
+  origin: `${envConfig.appMode == "production" ? "https://ifgames.bsospace.com" : "http://localhost:3000"}`,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
