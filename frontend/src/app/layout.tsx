a00937359e80b0a3d8766bc85a9d644005f0537e
Piyawat Wongyat <mask30254@gmail.com>
2025-01-28 23:19:50 +0700

    ✨ feat[toastify]: integrate react-toastify for custom toast notifications and update layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,25 +1,62 @@
-"use client";
+'use client'
 
-import React from "react";
-import './globals.css';
-import { AuthProvider } from "./hooks/useAuth";
+import React from 'react'
+import './globals.css'
+import { AuthProvider } from './hooks/useAuth'
+import { ToastContainer, Bounce } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout ({
+  children
+}: {
+  children: React.ReactNode
+}) {
   return (
     <AuthProvider>
-      <html lang="en">
-        <body className="flex flex-col min-h-screen bg-white text-gray-900">
+      <html lang='en'>
+        <ToastContainer
+          position='top-right'
+          autoClose={300}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme='colored'
+          transition={Bounce}
+        />
+        <body className='flex flex-col min-h-screen bg-white text-gray-900'>
           {children}
-          <footer className="mt-auto bg-gray-800 text-white py-4">
-            <div className="text-center text-sm">
+          <footer className='mt-auto bg-gray-800 text-white py-4'>
+            <div className='text-center text-sm'>
               <p>
-                Made with <span role="img" aria-label="love">❤️</span>&nbsp; by&nbsp;
-                <a href="https://www.instagram.com/smoif.buu" target="blank" className="hover:text-blue-700 hover:underline">SMOIF2024</a> and <a href="https://www.bsospace.com" target="blank" className="hover:text-blue-700 hover:underline">BSO Space</a>
+                Made with{' '}
+                <span role='img' aria-label='love'>
+                  ❤️
+                </span>
+                &nbsp; by&nbsp;
+                <a
+                  href='https://www.instagram.com/smoif.buu'
+                  target='blank'
+                  className='hover:text-blue-700 hover:underline'
+                >
+                  SMOIF2024
+                </a>{' '}
+                and{' '}
+                <a
+                  href='https://www.bsospace.com'
+                  target='blank'
+                  className='hover:text-blue-700 hover:underline'
+                >
+                  BSO Space
+                </a>
               </p>
             </div>
           </footer>
         </body>
       </html>
     </AuthProvider>
-  );
+  )
 }
