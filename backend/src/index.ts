009279a2973acb41fb9d8910249dcbc7c4fbb3bf
Piyawat Wongyat <mask30254@gmail.com>
2025-01-31 03:31:16 +0700

    ✨ feat: allow localhost access in CORS configuration for development

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: envConfig.appMode === "production"
-      ? ["https://ifgames.bsospace.com", "https://stagging-ifgames.bsospace.com"]
+      ? ["https://ifgames.bsospace.com", "https://stagging-ifgames.bsospace.com",'http://localhost:3000']
       : "http://localhost:3000",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
