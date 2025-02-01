5eb4bd93d4790bce660db4a4aa3cbe9871501b49
Piyawat Wongyat <mask30254@gmail.com>
2025-02-01 11:16:42 +0700

    🐛 fix: disable eslint rule for explicit any in interfaces

--- frontend/src/app/utils/interfaces.tsx
+++ frontend/src/app/utils/interfaces.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface SetScore {
     label: string;  // ชื่อของเซต เช่น "set1", "set2"
     score: number;  // คะแนนที่ได้ในเซตนั้น
